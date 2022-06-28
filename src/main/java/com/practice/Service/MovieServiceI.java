6dbcee67c7f7230156c12e935677cc52c5b24a6d
pdev0019@gmail.com <=>
2022-06-28 14:35:19 +0530

    Intial commit

---
+++ src/main/java/com/practice/Service/MovieServiceI.java
@@ -0,0 +1,23 @@
+package com.practice.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.demo.DTO.DirectorDTO;
+import com.demo.DTO.MovieDTO;
+import com.demo.Entity.Movie;
+
+public interface MovieServiceI {
+
+	
+	//Add,searchBasedOnTitle or else Invalid Title,updateReleaseDate,deleteBasedOnMovieTitle,displayAllMovies
+     void add(MovieDTO dto,List<DirectorDTO> directorDTO);
+     void  searchBasedOnTitle(String title);
+     void updateReleaseDate(LocalDate releaseDate,String movieTitle);
+     void deleteBasedOnMovieTitle(String Title);
+     List<Movie> displayAll();
+     
+     void getDirectorListFromTitle(String title);
+     void getMovieListFromDirectorName(String name);
+    
+}
